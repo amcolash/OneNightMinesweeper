3ccfd18b69c6d9d1f0cce7e90098c873c92e632f
Andrew McOlash <amcolash@gmail.com>
2017-01-23 21:01:37 -0600

    Work on adding in some more logic to game, generate grid and clicking!

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 window.onload = function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
-  var width = 30;
-  var height = 30;
+  var grid_width = 20;
+  var grid_height = 20;
+  var tile_width = 32;
+  var tile_height = 32;
+  var game_width = ((grid_width + 1) * tile_width) + (grid_width * 3);
+  var game_height = ((grid_height + 1) * tile_height) + (grid_height * 3);
+
+  var MINE = 9;
+  var FLAG = 10;
+
+  var grid = createArray(grid_height, grid_width);
+  var gameOverText;
+
+  var game = new Phaser.Game(game_width, game_height, Phaser.AUTO, '', { preload: preload, create: create });
 
   function preload () {
     game.load.image('0', 'img/0.png');
@@ -21,18 +32,80 @@ window.onload = function() {
   }
 
   function create () {
-    var grid = createArray(2, 2);
+    game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
 
-    for (var x = 0; x < width; x++) {
-      for (var y = 0; y < height; y++) {
-        grid[x][y] = 0;
+    var group = game.add.group();
+
+    for (var x = 0; x < grid_width; x++) {
+      for (var y = 0; y < grid_height; y++) {
+        var value = Math.floor(Math.random() * 10);
+        var tile = game.make.image(0, 0, "unknown");
+        tile.value = value;
+        tile.shown = false;
+        tile.inputEnabled = true;
+        tile.events.onInputDown.add(handler, this);
+        tile.width = 32;
+        tile.height = 32;
+
+        grid[x][y] = tile;
+        group.add(tile);
       }
     }
 
-    console.log(grid);
+    group.align(grid_width, -1, tile_width + 1, tile_height + 1);
+    group.position.x = (game.width - group.width) / 2;
+    group.position.y = (game.height - group.height) / 2;
 
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    gameOverText = game.add.text(0, 0, '', { fontSize: 64, fill: '#ffffff', stroke: 'black',
+      strokeThickness: 3, align: 'center' });
+
+    gameOverText.anchor.x = 0.5;
+    gameOverText.anchor.y = 0.5;
+    gameOverText.position.x = game.width / 2;
+    gameOverText.position.y = game.height / 2;
+  }
+
+  function handler(tile, pointer) {
+    if (pointer.leftButton.isDown && !tile.shown) {
+      var image = getImage(tile);
+      tile.loadTexture(image, 0, false);
+      tile.shown = true;
+
+      if (tile.value == MINE) {
+        gameOver();
+      }
+    } else if (pointer.rightButton.isDown && !tile.shown) {
+      tile.hasFlag = !tile.hasFlag;
+      tile.loadTexture(tile.hasFlag ? "flag" : "unknown", 0, false);
+    }
+  }
+
+  function gameOver() {
+    for (var x = 0; x < grid_width; x++) {
+      for (var y = 0; y < grid_height; y++) {
+        var tile = grid[x][y];
+        var image = getImage(tile);
+        tile.loadTexture(image, 0, false);
+      }
+    }
+
+    gameOverText.text = 'Game Over!';
+  }
+
+  function getImage(tile) {
+    switch(tile.value) {
+      case 0:   return "0";
+      case 1:   return "1";
+      case 2:   return "2";
+      case 3:   return "3";
+      case 4:   return "4";
+      case 5:   return "5";
+      case 6:   return "6";
+      case 7:   return "7";
+      case 8:   return "8";
+      case MINE:  return "mine";
+      default:  return "unknown";
+    }
   }
 
   function createArray(length) {
